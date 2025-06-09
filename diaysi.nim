2a743b700f2b778f7a494f32026f104b82e10fdd
Auric Vente <mprops@protonmail.com>
2025-06-08 18:47:41 -0600

    nothing

--- diaysi.nim
+++ diaysi.nim
@@ -3,6 +3,7 @@
 # When the program is called, it will show the HOUR (H).
 # Then it will delete that after x milliseconds.
 # Then it will print the MINUTES (M) for x milliseconds.
+# Then it will print the SECONDS (S) for x milliseconds.
 # Then it will clear it as well or exit.
 # It is temporary information.
 
@@ -10,11 +11,14 @@ import times
 import terminal
 import os
 
+let delay = 1000  # milliseconds to display each part
+
 proc main() =
   # Get current time
   let currentTime = now()
   let hour = currentTime.hour
   let minute = currentTime.minute
+  let second = currentTime.second
 
   # Initialize terminal
   hideCursor()
@@ -22,7 +26,7 @@ proc main() =
   # Display hour
   stdout.write $hour
   stdout.flushFile()
-  sleep(1000)
+  sleep(delay)
 
   # Clear just the number, not the whole line
   let digits = ($hour).len
@@ -35,12 +39,23 @@ proc main() =
   # Display minute
   stdout.write $minute
   stdout.flushFile()
-  sleep(1000)
+  sleep(delay)
 
-  # Clear final output and restore cursor
+  # Clear minute
   let minuteDigits = ($minute).len
-
   for i in 1..minuteDigits:
+    stdout.write("\b \b")
+  stdout.flushFile()
+
+  # Display second
+  stdout.write $second
+  stdout.flushFile()
+  sleep(delay)
+
+  # Clear second and restore cursor
+  let secondDigits = ($second).len
+
+  for i in 1..secondDigits:
     stdout.write("\b \b")
 
   stdout.flushFile()
